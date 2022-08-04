fa63f4f25675e3b819e9f955104aaccb10980d63
Aliasghar Asgharinia <a.asgharinia@yahoo.com>
2022-08-04 21:57:18 +0200

    Initial

---
+++ Project1/src/WelcomeScreen.java
@@ -0,0 +1,19 @@
+public class WelcomeScreen {
+    private String developerName;
+    private String applicationName;
+    public WelcomeScreen(){
+        this.applicationName = "List Guidance";
+        this.developerName = "Aliasghar Asgharinia";
+
+    }
+    public String getDeveloperName(){
+        return developerName;
+    }
+    public String getApplicationName(){
+        return applicationName;
+    }
+    public void welcomeMassage(){
+        System.out.println("\nHi Dear User, Lockers Pvt. Ltd is happy to welcome newly developed application");
+        System.out.println(applicationName+" was developed by "+developerName);
+    }
+}
